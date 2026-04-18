0b6bc19e10a314160593f09f55e18d59c66e31f7
mcarlod <carlo.diesta@gmail.com>
2026-04-18 12:22:48 +0930

    Modified useVapi to gather the api key from env

--- hooks/useVapi.ts
+++ hooks/useVapi.ts
@@ -18,6 +18,7 @@ const useLatestRef = <T>(value: T) => {
 }
 
 const VAPI_API_KEY = process.env.NEXT_PUBLIC_VAPI_API_KEY;
+
 const TIMER_INTERVAL_MS = 1000;
 const SECONDS_PER_MINUTE = 60;
 const TIME_WARNING_THRESHOLD = 60;

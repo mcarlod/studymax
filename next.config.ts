591e24044a37cd7af777ca6e185342489b25ea86
mcarlod <carlo.diesta@gmail.com>
2026-04-18 10:54:43 +0930

    Modified env keys

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    env: {
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    },
     images: { remotePatterns: [
         {
             protocol: 'https',
